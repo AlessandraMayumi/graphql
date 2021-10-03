d1599500f73b7153af556ad38d9cfe14ce72e399
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-10-03 18:56:27 -0300

    connect to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://user10:password10@cluster0.lrdkv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
